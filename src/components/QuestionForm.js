06de466302f2359ab61e7cd692aefa20b0dc10d9
Kris Johnson <krisrjohnson@gmail.com>
2020-01-07 15:54:12 -0500

    initial commit

---
+++ src/components/QuestionForm.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react'
+import _ from 'lodash'
+
+import TextField from './TextField'
+import ErrorList from './ErrorList'
+
+const QuestionForm = (props) => {
+  const [newQuestion, setNewQuestion] = useState({
+    question: "",
+    answer: ""
+  })
+
+  const [errors, setErrors] = useState({})
+
+  const clearForm = (event) => {
+    event.preventDefault()
+    setNewQuestion({
+      question: "",
+      answer: ""
+    })
+    setErrors({})
+  }
+
+  const handleQuestionFieldChange = (event) => {
+    setNewQuestion({
+      ...newQuestion,
+      [event.currentTarget.id]: event.currentTarget.value
+    })
+  }
+
+  const validForSubmission = () => {
+    let submitErrors = {}
+      const requiredFields = ["question", "answer"]
+      requiredFields.forEach((reqField) => {
+        if (newQuestion[reqField].trim() === "") {
+          submitErrors = {
+            ...submitErrors,
+            [reqField]: "cannot be blank"
+          }
+        }
+      })
+
+      setErrors(submitErrors)
+      return _.isEmpty(submitErrors)
+    }
+
+  const handleQuestionSubmit = (event) => {
+    event.preventDefault()
+    if (validForSubmission()) {
+      props.addNewQuestion(newQuestion)
+      clearForm()
+    }
+  }
+
+  return(
+    <div className="newQuestionForm">
+      <h3>Add A New Question</h3>
+      <form onSubmit={handleQuestionSubmit}>
+        <ErrorList errors={errors} />
+        <label className="newFormLabel"> Question: </label>
+        <TextField
+          fieldName="question"
+          id="question"
+          content={newQuestion.question}
+          handleChange={handleQuestionFieldChange}
+        />
+
+        <label className="newFormLabel"> Answer: </label>
+        <TextField
+          fieldName="answer"
+          id="answer"
+          content={newQuestion.answer}
+          handleChange={handleQuestionFieldChange}
+        />
+        <input
+          className="button"
+          type="submit"
+          value="Add Question"
+        />
+      </form>
+    </div>
+  )
+}
+
+export default QuestionForm
