4b9b30b9afa7be19cbb7e0bcd59c459f257f5cfd
Kris Johnson <krisrjohnson@gmail.com>
2020-01-09 13:06:12 -0500

    added link functionality to show page

--- src/components/LauncherShowPage.js
+++ src/components/LauncherShowPage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from "react-router-dom"
 
 const LauncherShowPage = (props) => {
   const [launcher, setLauncher] = useState(0)
@@ -15,11 +16,11 @@ const LauncherShowPage = (props) => {
   }, [])
 
   return(
-    <div>
+    <div className="text-center">
       <h1 className="bioHeader">{launcher.name}</h1>
       <div className="bio">{launcher.bio}</div>
+      <Link className="bio" to="/launchers">Back to full Launcher List</Link>
     </div>
-
   )
 }
 
